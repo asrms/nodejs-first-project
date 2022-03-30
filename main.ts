4b23c2cb0faefee8b151c0e3bf553079837d9b75
asrms <markoways@gmail.com>
2022-03-30 18:36:00 +0200

    first commit

---
+++ main.ts
@@ -0,0 +1,29 @@
+console.log("Hello World")
+
+console.log("This is my first project")
+
+let myVar = 3
+
+myVar += 10;
+
+console.log(myVar)
+
+
+
+interface MyInterface {
+    myNumber: number,
+    myString: string
+}
+
+
+let myInterface: MyInterface = {
+    myNumber: 10,
+    myString: "ciao"
+};
+
+
+  console.log(myInterface.myNumber)
+
+  console.log(myInterface.myString)
+
+
